508a1893a9f3b2c08e8a414b42028ba0608dcfd3
Campiotti <adriano.campiotti@gmail.com>
2018-05-17 11:53:07 +0200

    downsized video and forced it to be above text so it's click-able.

--- Route.php
+++ Route.php
@@ -67,7 +67,7 @@ class Route
             <body onload="shuffle()">
                 <div>
                     <img src="http://i0.kym-cdn.com/photos/images/original/001/117/432/515.gif" style="width:100%; height:100%; z-index: -1;">
-                    <div id="player" style="z-index: 5; position: absolute; right: 0; bottom: 0; max-height: 50%; max-width: 50%;"></div>
+                    <div id="player" style="z-index: 599; position: absolute; right: 0; bottom: 0; max-height: 25%; max-width: 25%;"></div>
                     <!--<iframe id="video" width="560" height="315" src="https://www.youtube-nocookie.com/embed/?autoplay=1&amp;rel=0&amp;controls=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen style="z-index: 2; position: absolute; right: 0; bottom: 0; max-height: 50%; max-width: 50%;"></iframe>-->
                     <img src="https://emojipedia-us.s3.amazonaws.com/thumbs/120/apple/129/face-screaming-in-fear_1f631.png" id="image" style="position: absolute; z-index: 255; top: 25%; height: 10%; width:10%; left:25%; overflow: hidden;">
                     <div style="z-index: 25; font-size: 4.20cm; top: 0; left: 0; position: absolute;">Page not found
@@ -91,8 +91,8 @@ class Route
                 var player;
                 function onYouTubeIframeAPIReady() {
                     player = new YT.Player('player', {
-                        height: '720',
-                        width: '1280',
+                        height: '480',
+                        width: '720',
                         videoId: '<?php echo $videoArr[array_rand($videoArr)]?>',
                         events: {
                             'onReady': onPlayerReady
