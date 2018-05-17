71a806b5df2927f85966bf71e66023c777fb8a95
Campiotti <adriano.campiotti@gmail.com>
2018-05-17 13:22:01 +0200

    added 🤔

--- Route.php
+++ Route.php
@@ -71,7 +71,7 @@ class Route
                     <!--<iframe id="video" width="560" height="315" src="https://www.youtube-nocookie.com/embed/?autoplay=1&amp;rel=0&amp;controls=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen style="z-index: 2; position: absolute; right: 0; bottom: 0; max-height: 50%; max-width: 50%;"></iframe>-->
                     <img src="https://emojipedia-us.s3.amazonaws.com/thumbs/120/apple/129/face-screaming-in-fear_1f631.png" id="image" style="position: absolute; z-index: 255; top: 25%; height: 10%; width:10%; left:25%; overflow: hidden;">
                     <div style="z-index: 25; font-size: 4.20cm; top: 0; left: 0; position: absolute;">Page not found
-                        <span id="emoji">😂😂😂🤣🤣🤣😂😂😂😭😭😭😱😱</span>
+                        <span id="emoji">😂😂😂🤣🤣🤣🤔🤔🤔😭😭😭😱😱</span>
                 </div>
             </body>
             <script>
